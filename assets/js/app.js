6081c64a3e620fa71a226aa6e4b77423c2d73f36
Todd Croak-Falen <toddcf@gmail.com>
2017-12-24 00:07:18 -0800

    Added fade out feature when an element is clicked to be removed.

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,19 @@ $( "li" ).on( "click", function() {
 	$( this ).toggleClass( "completed" );
 
 });
+
+// Click X to delete:
+$( "span" ).on( "click", function( event ) {
+
+	// Fade out the parent element of the span, which is the entire li:
+	$( this ).parent().fadeOut( 500, function() {
+
+		// AFTER it fades out, remove it so it's no longer slowing down the computer:
+		$( this ).remove();
+
+	});
+	// Prevent event bubbling so that the parent element (li, etc.) does not register the click event:
+	event.stopPropagation();
+	// $( this ).
+
+});
